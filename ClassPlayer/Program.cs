0c01a6dd58c7322291bb381df1b172075c0f990e
D Egor <Mail.egor.d@gmail.com>
2022-11-10 00:19:31 +0300

    Добавьте файлы проекта.

---
+++ ClassPlayer/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Programm
+{
+    class MyProgramm
+    {
+        static void Main()
+        {
+            Player player = new Player();
+
+            player.ShowStatus();
+        }
+    }
+
+    class Player
+    {
+        public int Health;
+        public int Damage;
+
+        public Player(int health, int damage)
+        {
+            Health = health;
+            Damage = damage;
+        }
+
+        public Player()
+        {
+            Health = 100;
+            Damage = 10;
+        }
+    
+        public void ShowStatus()
+        {
+            Console.WriteLine("Health: " + Health + "\nDamage: " + Damage);
+        }
+    }
+}
